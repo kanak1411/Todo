e510cdf765273bc82daa91d00ec9f3d37bf5345f
Kanak Agarwal <kanak1411@gmail.com>
2020-08-30 21:36:23 +0530

    Updates

---
+++ precache-manifest.b5c060e1ba5705f1a2a7e90e094328ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7850d25d79b0d0a242349ea5fa6e437",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "63a06cb1d9e301f67413",
+    "url": "/Todo/static/css/main.08e3a2aa.chunk.css"
+  },
+  {
+    "revision": "262c788b11e23e4bffdf",
+    "url": "/Todo/static/js/2.84396668.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo/static/js/2.84396668.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63a06cb1d9e301f67413",
+    "url": "/Todo/static/js/main.7843591b.chunk.js"
+  },
+  {
+    "revision": "162a65c0456d03b31aa2",
+    "url": "/Todo/static/js/runtime-main.e3579041.js"
+  }
+]);
